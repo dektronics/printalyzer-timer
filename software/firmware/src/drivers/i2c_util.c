8ef2b164ae62e6175e57425a958f3d21b3d5d3f2
Derek Konigsberg <dkonigsberg@logicprobe.org>
2020-12-04 16:22:59 -0800

    Changed util functions to use HAL mem functions
    At this point, these utility functions are almost unnecessary. Only
    keeping them because they're a bit shorter, and hide some of the pointer
    management overhead for single-byte writes.

--- software/firmware/src/drivers/i2c_util.c
+++ software/firmware/src/drivers/i2c_util.c
@@ -4,34 +4,10 @@
 
 HAL_StatusTypeDef i2c_read_register(I2C_HandleTypeDef *hi2c, uint16_t device_id, uint8_t reg, uint8_t *data)
 {
-    HAL_StatusTypeDef ret = HAL_OK;
-
-    if (!data) {
-        return HAL_ERROR;
-    }
-
-    do {
-        ret = HAL_I2C_Master_Transmit(hi2c, device_id, &reg, 1, HAL_MAX_DELAY);
-        if (ret != HAL_OK) {
-            break;
-        }
-
-        ret = HAL_I2C_Master_Receive(hi2c, device_id, data, 1, HAL_MAX_DELAY);
-
-    } while (0);
-
-    return ret;
+    return HAL_I2C_Mem_Read(hi2c, device_id, reg, I2C_MEMADD_SIZE_8BIT, data, 1, HAL_MAX_DELAY);
 }
 
 HAL_StatusTypeDef i2c_write_register(I2C_HandleTypeDef *hi2c, uint16_t device_id, uint8_t reg, uint8_t data)
 {
-    HAL_StatusTypeDef ret = HAL_OK;
-    uint8_t buf[2];
-
-    buf[0] = reg;
-    buf[1] = data;
-
-    ret = HAL_I2C_Master_Transmit(hi2c, device_id, buf, 2, HAL_MAX_DELAY);
-
-    return ret;
+    return HAL_I2C_Mem_Write(hi2c, device_id, reg, I2C_MEMADD_SIZE_8BIT, &data, 1, HAL_MAX_DELAY);
 }
