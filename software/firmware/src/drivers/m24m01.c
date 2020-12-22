5758d64e047ecaecaf7dc3fe57582d380db31d19
Derek Konigsberg <dkonigsberg@logicprobe.org>
2020-12-22 15:49:44 -0800

    Implement the buffer and page write functions for the I2C EEPROM

--- software/firmware/src/drivers/m24m01.c
+++ software/firmware/src/drivers/m24m01.c
@@ -8,48 +8,24 @@
 
 static const char *TAG = "m24m01";
 
+#define MIN(x,  y)   (((x) < (y)) ? (x) : (y))
+
 /* I2C device base address */
 static const uint8_t M24M01_ADDRESS = 0x50 << 1;
 
 #define DEVICE_ADDRESS(x) (M24M01_ADDRESS | (uint8_t)((x & 0x10000) >> 15))
 #define MEMORY_ADDRESS(x) ((uint16_t)(x & 0xFFFF))
 
-HAL_StatusTypeDef m24m01_init(I2C_HandleTypeDef *hi2c)
-{
-//    HAL_StatusTypeDef ret;
-//    uint8_t data[2];
-
-    //FIXME change this quick and dirty test code into something useful
-
-//    ESP_LOGI(TAG, "Testing M24M01...");
-//
-//    // Note: The read sequence for this chip might not be correct without testing.
-//    // At this point, we really just care that it is responding to I2C commands.
-//
-//    data[0] = 0x00;
-//    data[1] = 0x00;
-//    ret = HAL_I2C_Master_Transmit(hi2c, M24M01_ADDRESS, data, 2, HAL_MAX_DELAY);
-//    if (ret != HAL_OK) {
-//        ESP_LOGE(TAG, "Unable to write byte address");
-//        return ret;
-//    }
-//
-//    ret = HAL_I2C_Master_Receive(hi2c, M24M01_ADDRESS, data, 1, HAL_MAX_DELAY);
-//    if (ret != HAL_OK) {
-//        ESP_LOGE(TAG, "Unable to read data");
-//        return ret;
-//    }
-//
-//    ESP_LOGI(TAG, "Data at first address: %02X", data[0]);
-
-    return HAL_OK;
-}
-
 HAL_StatusTypeDef m24m01_read_byte(I2C_HandleTypeDef *hi2c, uint32_t address, uint8_t *data)
 {
     HAL_StatusTypeDef ret = HAL_OK;
 
     ESP_LOGD(TAG, "m24m01_read_byte: address=0x%05lX", address);
+
+    if (address > 0x1FFFF) {
+        ESP_LOGE(TAG, "Cannot read from invalid address");
+        return HAL_ERROR;
+    }
 
     uint8_t device_addr = DEVICE_ADDRESS(address);
     uint16_t mem_addr = MEMORY_ADDRESS(address);
@@ -68,6 +44,11 @@ HAL_StatusTypeDef m24m01_read_buffer(I2C_HandleTypeDef *hi2c, uint32_t address, 
 
     ESP_LOGD(TAG, "m24m01_read_buffer: address=0x%05lX, size=%d", address, data_len);
 
+    if (address > 0x1FFFF) {
+        ESP_LOGE(TAG, "Cannot read from invalid address");
+        return HAL_ERROR;
+    }
+
     uint8_t device_addr = DEVICE_ADDRESS(address);
     uint16_t mem_addr = MEMORY_ADDRESS(address);
 
@@ -85,10 +66,18 @@ HAL_StatusTypeDef m24m01_write_byte(I2C_HandleTypeDef *hi2c, uint32_t address, u
 
     ESP_LOGD(TAG, "m24m01_write_byte: address=0x%05lX", address);
 
+    if (address > 0x1FFFF) {
+        ESP_LOGE(TAG, "Cannot write to invalid address");
+        return HAL_ERROR;
+    }
+
     uint8_t device_addr = DEVICE_ADDRESS(address);
     uint16_t mem_addr = MEMORY_ADDRESS(address);
 
     ret = HAL_I2C_Mem_Write(hi2c, device_addr, mem_addr, I2C_MEMADD_SIZE_16BIT, &data, 1, HAL_MAX_DELAY);
+    if (ret != HAL_OK) {
+        ESP_LOGE(TAG, "HAL_I2C_Mem_Write error: %d", ret);
+    }
 
     while(HAL_I2C_IsDeviceReady(hi2c, device_addr, 1, HAL_MAX_DELAY) != HAL_OK);
 
@@ -97,6 +86,65 @@ HAL_StatusTypeDef m24m01_write_byte(I2C_HandleTypeDef *hi2c, uint32_t address, u
 
 HAL_StatusTypeDef m24m01_write_buffer(I2C_HandleTypeDef *hi2c, uint32_t address, const uint8_t *data, size_t data_len)
 {
-    //TODO making this work as expected may require some page handling
-    return HAL_ERROR;
+    HAL_StatusTypeDef ret = HAL_OK;
+
+    ESP_LOGD(TAG, "m24m01_write_buffer: address=0x%05lX, len=%d", address, data_len);
+
+    if (!data || data_len == 0) {
+        ESP_LOGE(TAG, "Data is null or empty");
+        return HAL_ERROR;
+    }
+
+    if (data_len > 0x1FFFF - address) {
+        ESP_LOGE(TAG, "Buffer exceeds memory boundary: 0x%05lX+%d > 0x%05lX",
+            address, data_len, 0x1FFFF - address);
+        return HAL_ERROR;
+    }
+
+    uint32_t offset = 0;
+    do {
+        size_t page_len = 0x100 - (uint8_t)((address + offset) & 0x000FF);
+        size_t write_len = MIN(page_len, data_len - offset);
+        ret = m24m01_write_page(hi2c, address + offset, data + offset, write_len);
+        offset += write_len;
+    } while (ret == HAL_OK && offset < data_len);
+
+    return ret;
+}
+
+HAL_StatusTypeDef m24m01_write_page(I2C_HandleTypeDef *hi2c, uint32_t address, const uint8_t *data, size_t data_len)
+{
+    HAL_StatusTypeDef ret = HAL_OK;
+
+    ESP_LOGD(TAG, "m24m01_write_page: address=0x%05lX, len=%d", address, data_len);
+
+    if (address > 0x1FFFF) {
+        ESP_LOGE(TAG, "Cannot write to invalid address");
+        return HAL_ERROR;
+    }
+
+    uint8_t device_addr = DEVICE_ADDRESS(address);
+    uint16_t mem_addr = MEMORY_ADDRESS(address);
+
+    if (!data || data_len == 0) {
+        ESP_LOGE(TAG, "Data is null or empty");
+        return HAL_ERROR;
+    }
+
+    uint8_t page_offset = (uint8_t)(mem_addr & 0x00FF);
+    if (data_len > 0x100 - page_offset) {
+        ESP_LOGE(TAG, "Buffer exceeds page boundary: 0x%05lX+%d > 0x%05lX",
+            address, data_len, ((address & 0x1FF00) + 0xFF));
+        return HAL_ERROR;
+    }
+
+    ret = HAL_I2C_Mem_Write(hi2c, device_addr, mem_addr, I2C_MEMADD_SIZE_16BIT,
+        (uint8_t *)data, data_len, HAL_MAX_DELAY);
+    if (ret != HAL_OK) {
+        ESP_LOGE(TAG, "HAL_I2C_Mem_Write error: %d", ret);
+    }
+
+    while(HAL_I2C_IsDeviceReady(hi2c, device_addr, 1, HAL_MAX_DELAY) != HAL_OK);
+
+    return ret;
 }
--- software/firmware/src/drivers/m24m01.h
+++ software/firmware/src/drivers/m24m01.h
@@ -7,12 +7,64 @@
 
 #include <stm32f4xx_hal.h>
 
-HAL_StatusTypeDef m24m01_init(I2C_HandleTypeDef *hi2c);
-
+/**
+ * Read a byte from the specified address.
+ *
+ * @param hi2c Pointer to a handle for the I2C peripheral
+ * @param address Address to read from
+ * @param data Pointer to the variable to read the data into
+ */
 HAL_StatusTypeDef m24m01_read_byte(I2C_HandleTypeDef *hi2c, uint32_t address, uint8_t *data);
+
+/**
+ * Read a sequence of bytes from the specified address.
+ *
+ * If the read size exceeds the available size of the memory beyond the
+ * starting address, then the read operation will wrap around and read
+ * from the beginning of the memory space.
+ *
+ * @param hi2c Pointer to a handle for the I2C peripheral
+ * @param address Address to read from
+ * @param data Pointer to the buffer to read the data into
+ * @param data_len Size of the data to be read
+ */
 HAL_StatusTypeDef m24m01_read_buffer(I2C_HandleTypeDef *hi2c, uint32_t address, uint8_t *data, size_t data_len);
 
+/**
+ * Write a byte to the specified address.
+ *
+ * @param hi2c Pointer to a handle for the I2C peripheral
+ * @param address Address to write to
+ * @param data Data to be written
+ */
 HAL_StatusTypeDef m24m01_write_byte(I2C_HandleTypeDef *hi2c, uint32_t address, uint8_t data);
+
+/**
+ * Write a sequence of bytes to the specified address.
+ *
+ * Actual write operations may be broken up into several individual page
+ * writes, and thus a failure may happen after a partial write was
+ * completed.
+ *
+ * @param hi2c Pointer to a handle for the I2C peripheral
+ * @param address Address to write from
+ * @param data Pointer to the buffer to write the data from
+ * @param data_len Size of the data to be written
+ */
 HAL_StatusTypeDef m24m01_write_buffer(I2C_HandleTypeDef *hi2c, uint32_t address, const uint8_t *data, size_t data_len);
+
+/**
+ * Write a sequence of bytes to the specified address within a memory page.
+ *
+ * Buffer write operations are limited to the 256-byte page of the starting
+ * address, and will fail if they exceed these bounds. To write across
+ * multiple pages, multiple write operations are necessary.
+ *
+ * @param hi2c Pointer to a handle for the I2C peripheral
+ * @param address Address to write from
+ * @param data Pointer to the buffer to write the data from
+ * @param data_len Size of the data to be written
+ */
+HAL_StatusTypeDef m24m01_write_page(I2C_HandleTypeDef *hi2c, uint32_t address, const uint8_t *data, size_t data_len);
 
 #endif /* M24M01_H */
