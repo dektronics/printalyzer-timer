8a42b459ed547fe82a1e92d52e09848382d196b9
Derek Konigsberg <dkonigsberg@dektronics.com>
2023-08-02 10:00:27 -0700

    Move enlarger calibration code to a separate file
    This is a mostly mechanical refactoring, as a starting point for future
    work on the calibration code. Since the enlarger configuration is
    significantly more involved than it used to be, it makes sense to take
    the calibration process and move it out into a separate file.

---
+++ software/firmware/src/enlarger_calibration.h
@@ -0,0 +1,10 @@
+#ifndef ENLARGER_CALIBRATION_H
+#define ENLARGER_CALIBRATION_H
+
+#include "main_menu.h"
+
+typedef struct __enlarger_config_t enlarger_config_t;
+
+menu_result_t menu_enlarger_calibration(enlarger_config_t *config);
+
+#endif /* ENLARGER_CALIBRATION_H */
