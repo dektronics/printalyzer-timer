51b83f02f8ae14504a6508aab29633be2de6d000
Derek Konigsberg <dkonigsberg@dektronics.com>
2023-10-22 23:11:23 -0700

    First pass at putting together a basic CMake based build process
    Right now this is mostly within a single configuration for the base
    project, and does not yet include the bootloader code. Eventually common
    elements could be refactored out.
    
    The idea is to make it possible to build the firmware outside of the
    Eclipse CDT managed build process, which has benefits for build servers,
    testing, and easier IDE flexibility.
    
    Also finally wrote a script to extract binary size and free space
    information out of the build output. This requires using the GNU
    binutils "size" command, and parsing various bits of information from it
    output.  The complexity here is unfortunately necessary, because our
    binaries are intentionally padded out to the full flash size to support
    checksums.
    
    Some IDE config files for CLion have also been added, but setup
    instructions are not yet written.

---
+++ software/firmware/tools/codesize.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+
+# Collect the command line arguments
+my $size_command = "size";
+GetOptions('size=s' => \$size_command);
+
+my $elf_file = $ARGV[0] or die "Missing ELF file parameter\n";
+
+# Run "size" two ways to collect all the information
+my $size_output_sum = `$size_command -B -d $elf_file`;
+my $size_output_sec = `$size_command -A -d $elf_file`;
+
+# Parse the total FLASH size out of the Berkeley format
+my @sizes = $size_output_sum =~ /^\s*(\d+)\s+(\d+)\s+/m;
+
+if (scalar(@sizes) != 2) {
+    die "Malformed size output\n";
+}
+
+my $flash_size = int($sizes[0]) + int($sizes[1]);
+
+# Parse the individual section sizes out of the SysV format
+my %sections;
+foreach my $line (split /^/m, $size_output_sec) {
+    my @elements = $line =~ /^\.(\S+)\s+(\d+)\s+\d+$/m;
+    if (scalar(@elements) == 2) {
+        $sections{$elements[0]} = $elements[1];
+    }
+}
+
+# Print the results
+printf("Free space: %d (%d KB)\n", $sections{'end_fill'}, $sections{'end_fill'} / 1024);
+printf("Total size: %d (%d KB)\n", $flash_size, $flash_size / 1024);
