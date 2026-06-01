1f601b91f0bab82584d46ff13c408822cdaf75e2
Derek Konigsberg <dkonigsberg@dektronics.com>
2026-06-01 12:57:35 -0700

    Set the FatFs file time based on the build timestamp

--- software/firmware/src/app_descriptor.c
+++ software/firmware/src/app_descriptor.c
@@ -1,5 +1,11 @@
 #include "app_descriptor.h"
 
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "ff.h"
+
 const __attribute__((section(".app_descriptor"))) app_descriptor_t app_descriptor = {
     .magic_word = APP_DESCRIPTOR_MAGIC_WORD,
     .project_name = "Printalyzer",
@@ -18,3 +24,35 @@ const app_descriptor_t *app_descriptor_get()
 {
     return &app_descriptor;
 }
+
+#ifdef APP_BUILD_DATE
+DWORD get_fattime (void)
+{
+    /*
+     * This function assumes that APP_BUILD_DATE has been set
+     * with the exact format of "YYYY-MM-DD HH:MM".
+     */
+
+    /* Copy the build timestamp into an array of null-separated fields */
+    char str[20] = {0};
+    memcpy(str, APP_BUILD_DATE, sizeof(APP_BUILD_DATE));
+    str[4] = '\0';
+    str[7] = '\0';
+    str[10] = '\0';
+    str[13] = '\0';
+
+    /* Parse out the timestamp components */
+    const int tm_year = strtol(str, NULL, 10) - 1900;
+    const int tm_mon = strtol(str + 5, NULL, 10) - 1;
+    const int tm_mday = strtol(str + 8, NULL, 10);
+    const int tm_hour = strtol(str + 11, NULL, 10);
+    const int tm_min = strtol(str + 14, NULL, 10);
+
+    /* Return the FatFs timestamp value */
+    return (DWORD)(tm_year - 80) << 25 |
+           (DWORD)(tm_mon + 1) << 21 |
+           (DWORD)tm_mday << 16 |
+           (DWORD)tm_hour << 11 |
+           (DWORD)tm_min << 5;
+}
+#endif
